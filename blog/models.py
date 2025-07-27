98d3e89128240c1f7d7667c65f903d910d664119
Amirali Kavoossi <kavoossi1co@gmail.com>
2025-07-27 19:06:33 +0330

    database change

--- blog/models.py
+++ blog/models.py
@@ -4,3 +4,7 @@ from django.db import models
 class post (models.Model):
     title=models.CharField(max_length=250)
     content=models.TextField()
+    status=models.BooleanField(default=0)
+def __str__(self):
+    return self.id
+    #return '{}_{}'.format(self.name,self.id)
