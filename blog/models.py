0049c3781ac7b74f0e12523a1932c26d896e09d9
Amirali Kavoossi <kavoossi1co@gmail.com>
2025-08-02 17:11:33 +0330

    database change

--- blog/models.py
+++ blog/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class post (models.Model):
     title=models.CharField(max_length=250)
     content=models.TextField()
-    status=models.BooleanField(default=0)
+    status=models.BooleanField(default=1)
 def __str__(self):
     return self.id
     #return '{}_{}'.format(self.name,self.id)
--- mywebsite/views.py
+++ mywebsite/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from blog.models import post
 # Create your views here.
 from django.http import HttpResponse,JsonResponse
 def index_view(request):
@@ -7,3 +8,7 @@ def about_view(request):
     return render(request,'website/about.html')
 def contact_view(request):
     return render(request,'website/contact.html')
+def test_view(request):
+    posts=post.objects.all()
+    contex={'posts':posts}
+    return render(request,'website/test.html',contex)
