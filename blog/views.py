abba740a39188457f643ad0bed476183ec5bc6c7
Amirali Kavoossi <kavoossi1co@gmail.com>
2025-08-29 21:29:09 +0330

    pagination changes

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,16 @@
-from django.shortcuts import render,get_object_or_404,get_list_or_404
+from django.shortcuts import render,get_object_or_404
+from django.core.paginator import Paginator,EmptyPage
 from django.utils import timezone
 from blog.models import post
 # Create your views here.
 def blog_view(request):
     posts=post.objects.filter(published_date__lte=timezone.now(),status=1)
+    posts=Paginator(posts,3)
+    try:
+        page_number=request.GET.get('page')
+        posts=posts.get_page(page_number)
+    except EmptyPage :
+        posts=posts.get_page(1)
     return render(request,'blog/blog-home.html',{'posts':posts})
 def single_view(request,pid):
     postt=get_object_or_404(post,pk=pid,published_date__lte=timezone.now(),status=1)
