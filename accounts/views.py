65591bc12a82c515ec44d2c0abc3de5d1a4d092b
Amirali Kavoossi <kavoossi1co@gmail.com>
2025-09-10 12:56:00 +0330

    forgot pass changes

--- accounts/views.py
+++ accounts/views.py
@@ -3,24 +3,21 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import AuthenticationForm 
 from django.contrib.auth.decorators import login_required
 from accounts.forms import CustomUserCreationForm
+from django.contrib import messages
 
 def login_view(request):
     if request.user.is_authenticated:
         return redirect('/')
-
     if request.method == 'POST':
-        form = AuthenticationForm(request=request, data=request.POST)
-        if form.is_valid():
-            username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password')
-            user = authenticate(request, username=username, password=password)
-            if user is not None:
-                login(request, user)
-                return redirect('/')
-    else:
-        form = AuthenticationForm()
-
-    return render(request, 'accounts/login.html', {'form': form})
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('/')
+        else:
+            messages.error(request, "Your username or password is not correct.")
+    return render(request, 'accounts/login.html')
 
 @login_required
 def logout_view(request):
--- mysite/settings.py
+++ mysite/settings.py
@@ -155,3 +155,13 @@ AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend', 
 ]
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+#send email
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = "kavoossi1co@gmail.com"   
+EMAIL_HOST_PASSWORD = "xznhfknpeummokdr"  
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
