d03d5123b82d2703cdc3bb66ea35793deb195c03
Amirali Kavoossi <kavoossi1co@gmail.com>
2025-09-18 02:26:18 +0330

    liara changes 4

--- mywebsite/urls.py
+++ mywebsite/urls.py
@@ -5,7 +5,7 @@ from mywebsite.views import *
 app_name='website'
 
 urlpatterns = [
-#    re_path(r'^.*$', coming_view),
+    re_path(r'^.*$', coming_view),
     path('', index_view,name='index'),
     path('about/',about_view,name='about'),
     path('contact/',contact_view,name='contact'),
